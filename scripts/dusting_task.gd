8ee8789fcf513bb9d1a8d3bb7962f728289b03b9
Sohan Saha <sohansaha2011@gmail.com>
2026-09-20 16:23:13 +0530

    [dusting] complete dust and spider web clearing with HUD progress tracking

--- scripts/dusting_task.gd
+++ scripts/dusting_task.gd
@@ -6,21 +6,44 @@ signal dusting_task_completed
 @onready var dust_spots: Array = $DustSpots.get_children()
 @onready var spider_webs: Array = $SpiderWebs.get_children()
 
-var cleaned_count: int = 0
+var dust_cleaned: int = 0
+var webs_cleaned: int = 0
 var total_count: int = 0
+var cleaned_count: int = 0
+
 
 func _ready() -> void:
 	add_to_group("dusting_task")
 	total_count = dust_spots.size() + spider_webs.size()
+	_update_hud()
+
 
 func clean_spot(dirty_spot: Area3D) -> void:
-	if dirty_spot.visible:
-		dirty_spot.visible = false
-		dirty_spot.get_node("CollisionShape3D").disabled = true
-		cleaned_count += 1
-		_check_completion()
+	print("clean_spot called on: ", dirty_spot.name)
+	if not dirty_spot.visible:
+		return
+
+	dirty_spot.visible = false
+	dirty_spot.get_node("CollisionShape3D").disabled = true
+	cleaned_count += 1
+
+	if dirty_spot in dust_spots:
+		dust_cleaned += 1
+	elif dirty_spot in spider_webs:
+		webs_cleaned += 1
+
+	_update_hud()
+	_check_completion()
+
+
+func _update_hud() -> void:
+	var hud := get_tree().get_first_node_in_group("hud")
+	print("hud found: ", hud)
+	if hud:
+		hud.update_dusting(dust_cleaned, dust_spots.size())
+		hud.update_webs(webs_cleaned, spider_webs.size())
+
 
 func _check_completion() -> void:
 	if cleaned_count >= total_count:
-		print("Dusting task completed!")
 		emit_signal("dusting_task_completed")
--- scripts/hud.gd
+++ scripts/hud.gd
@@ -4,9 +4,17 @@ extends CanvasLayer
 @onready var interaction_label: Label = $InteractionPrompt/Label
 @onready var task_label: Label = $TaskBoard/TaskLabel
 
+var furniture_done: int = 0
+var furniture_total: int = 1
+var dusting_done: int = 0
+var dusting_total: int = 6
+var webs_done: int = 0
+var webs_total: int = 4
+
 
 func _ready() -> void:
 	set_interaction_prompt("")
+	_refresh_task_board()
 
 
 func set_interaction_prompt(prompt_text: String) -> void:
@@ -15,5 +23,26 @@ func set_interaction_prompt(prompt_text: String) -> void:
 
 
 func mark_sofa_complete() -> void:
-	task_label.text = "FURNITURE                  1/1"
+	furniture_done = furniture_total
+	_refresh_task_board()
 	set_interaction_prompt("TASK COMPLETE")
+
+
+func update_dusting(done: int, total: int) -> void:
+	dusting_done = done
+	dusting_total = total
+	_refresh_task_board()
+
+
+func update_webs(done: int, total: int) -> void:
+	webs_done = done
+	webs_total = total
+	_refresh_task_board()
+
+
+func _refresh_task_board() -> void:
+	task_label.text = "FURNITURE                  %d/%d\n\nDUSTING                    %d/%d\nSPIDER WEBS                %d/%d" % [
+		furniture_done, furniture_total,
+		dusting_done, dusting_total,
+		webs_done, webs_total
+	]
